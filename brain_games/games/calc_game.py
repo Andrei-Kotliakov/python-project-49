075cb0815823cd71e73cc3e88ea76881775d9c26
Andrei-Kotliakov <andreikotliakov@yandex.ru>
2024-07-31 22:15:12 +0300

    Added dir games and added calc_game

---
+++ brain_games/games/calc_game.py
@@ -0,0 +1,21 @@
+import random
+from random import randint
+from random import choice
+
+
+def information():
+    num_1 = randint(1, 10)
+    num_2 = randint(1, 10)
+    math_operators = ['+', '-', '*']
+    operator = choice(math_operators)
+    question = (f'{num_1} {operator} {num_2}')
+    if operator == '+':
+        correct_answer = num_1 + num_2
+    elif operator == '-':
+        correct_answer = num_1 - num_2
+    else:
+        correct_answer = num_1 * num_2
+    return question, str(correct_answer)
+
+
+
