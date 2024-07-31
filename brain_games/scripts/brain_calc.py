2c10a31a4a0c496e535916be2fbd4b7088db9c9f
Andrei-Kotliakov <andreikotliakov@yandex.ru>
2024-07-31 22:12:41 +0300

    Added brain_calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+from brain_games.body import play
+from brain_games.games import calc_game
+
+
+def main():
+    play(calc_game)
+
+
+
+if __name__ == '__main__':
+    main()
+
