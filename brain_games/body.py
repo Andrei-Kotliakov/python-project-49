b13f7eda0af8ae512b4db834dd33cf1564069dc5
Andrei-Kotliakov <andreikotliakov@yandex.ru>
2024-07-31 22:11:25 +0300

    Added body.py

---
+++ brain_games/body.py
@@ -0,0 +1,24 @@
+import prompt
+
+
+def play(calculator):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('What is the result of the expression?')
+    tries_count = 3
+    while tries_count:
+        question, correct_answer = calculator.information()
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+        if answer == str(correct_answer):
+            print('Correct')
+            tries_count -= 1
+        else:
+            print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}. Let\'s try again, {name}!')
+            return
+    print(f'Congratulations, {name}!')
+
+        
+
+
