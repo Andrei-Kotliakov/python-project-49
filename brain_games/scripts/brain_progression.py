f0e60e239dc5812379bbd025c313b700b66adaf4
Andrei-Kotliakov <andreikotliakov@yandex.ru>
2024-08-02 21:39:57 +0300

    Added brain_progression

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.body import play
+from brain_games.games import progression_game
+
+
+def main():
+    play(progression_game)
+
+
+if __name__ == '__main__':
+    main()
