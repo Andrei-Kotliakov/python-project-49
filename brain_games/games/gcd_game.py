1a1dc2359e1e165c74af2efa6de6b5fdcd9ff9f1
Andrei-Kotliakov <andreikotliakov@yandex.ru>
2024-08-04 22:04:33 +0300

    fixed smth

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -10,4 +10,4 @@ def make_rules():
     num_2 = randint(1, 100)
     question = (f'{num_1} {num_2}')
     correct_answer = math.gcd(num_1, num_2)
-    return question, str(correct_answer)
+    return question, correct_answer
